93f0c330e2f3cbc6a914607573006761cad32a83
oshevchuk0111 <o.shevchuk0111@gmail.com>
2026-03-14 18:04:10 +0100

    injecting not services value

--- src/app/tasks/task.model.ts
+++ src/app/tasks/task.model.ts
@@ -1,4 +1,39 @@
+import { InjectionToken, Provider } from '@angular/core';
+
 export type TaskStatus = 'OPEN' | 'IN_PROGRESS' | 'DONE';
+
+type TaskStatusOption = {
+  value: 'open' | 'in-progress' | 'done';
+  taskStatus: TaskStatus;
+  text: string;
+}[];
+
+export const TASK_STATUS_OPTIONS = new InjectionToken<TaskStatusOption>(
+  'task-status-options',
+);
+
+export const TaskStatusOption: TaskStatusOption = [
+  {
+    value: 'open',
+    taskStatus: 'OPEN',
+    text: 'Open',
+  },
+  {
+    value: 'in-progress',
+    taskStatus: 'IN_PROGRESS',
+    text: 'In-Progress',
+  },
+  {
+    value: 'done',
+    taskStatus: 'DONE',
+    text: 'Completed',
+  },
+];
+
+export const taskStatusOptionsProvider: Provider = {
+  provide: TASK_STATUS_OPTIONS,
+  useValue: TaskStatusOption,
+};
 
 export interface Task {
   id: string;
